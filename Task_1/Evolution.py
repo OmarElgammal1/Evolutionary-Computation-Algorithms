7f6af14c26aeea39034eef45c0595b19b62eb378
bieno12 <zeyadshahin2015@gmail.com>
2024-05-03 22:28:26 +0300

    log generations into log file

--- Task_1/Evolution.py
+++ Task_1/Evolution.py
@@ -1,4 +1,4 @@
-import random, time, pygame, sys
+import random, json
 from pygame.locals import *
 from Agent import Agent
 from Environment import Environment, GameEngine
@@ -9,7 +9,7 @@ class Evolution:
 		self.mutationRate = mutationRate
 		self.population = []
 		self.generateRandomPopulation()
-
+		self.generation_logs = []
 
 	def generateRandomPopulation(self):
 		self.population = []
@@ -45,4 +45,15 @@ class Evolution:
 
 			for env in self.engine.environments:
 				env.agent.fitness = env.score
+			self.generation_logs.append(
+				sorted(
+					[{'chromosome': agent.chromosome, 'fitness': agent.fitness}
+	  					for agent in self.population], key=lambda x: x['fitness'], reverse=True))
+			self.save_generation_log()
 			self.nextGeneration()
+
+	def save_generation_log(self):
+		with open("generations.json", 'w') as file:
+			json.dump(self.generation_logs, file, indent=4)
+
+
