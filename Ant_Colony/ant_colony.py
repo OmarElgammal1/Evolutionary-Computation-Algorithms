ad40f43825a9c36ba62141b21b1133e87101a362
Shehab Diab <shehabgehaddiab@gmail.com>
2024-05-07 19:39:10 +0300

    add visualizer.py

--- Ant_Colony/ant_colony.py
+++ Ant_Colony/ant_colony.py
@@ -1,8 +1,7 @@
 from graph import TSPGraph
 import adjacency_mat_generator
 from random import randint
-# from manim import *
-
+from manim import *
 
 class AntColonyOptimization:
     def __init__(self, adj_mat, n_cities, alpha=1, beta=1):
@@ -43,89 +42,3 @@ class AntColonyOptimization:
         if cycle_cost < best_cost:
             return cycle_cost, cycle
         return best_cost, best_cycle
-
-
-# class AntColonyVisualizer(Scene):
-#     def __init__(self, adj_mat, n_cities, alpha=1, beta=1):
-#         super().__init__()
-#         self.graph = TSPGraph(adj_mat, n_cities, alpha=alpha, beta=beta)
-#         self.n_cities = n_cities
-#         self.nodes = [i for i in range(self.n_cities)]
-#         self.edges = self.__make_edges(self.n_cities)
-
-#     def __make_edges(self, n):
-#         edges = []
-#         for i in range(n):
-#             for j in range(i+1, n):
-#                 edges.append((i, j))
-                
-#         return edges
-        
-#     def construct(self):
-#         g = Graph(self.nodes, self.edges, layout="spring", layout_scale=12,
-#                   labels=True, vertex_config={'radius':0.5})
-
-#         scene_width = 40
-#         scene_height = 30
-#         self.camera.frame_width = scene_width        
-#         self.camera.frame_height = scene_height
-#         top_center = [0, scene_height / 2, 0]
-
-#         N_ANTS = 20
-#         N_ITERS = 10
-#         ant_dots = [Dot(color=ManimColor.from_hex("#FF0000")) for _ in range(N_ANTS)]
-#         node_positions = {label: dot.get_center() for label, dot in g.vertices.items()}
-
-#         best_cycle, best_cost, traversal_history = self.optimize(n_iters=N_ITERS, n_ants=N_ANTS, degradation_factor=0.8)
-#         for i in range(N_ITERS):
-#             iteration_number = Tex(f"Iteration {i}").set_position(top_center)
-#             nth_iteration_cycles = traversal_history[i]
-#             for j in range(len(nth_iteration_cycles)):
-#                 jth_ant = nth_iteration_cycles[j]
-#                 print(jth_ant)
-
-
-#         # SRC_NODE = 0
-#         # ant = Dot(color=random_color())
-#         # ant = Dot(node_positions[SRC_NODE], color=RED)
-#         self.play(Create(g))
-#         # self.play(ant.animate.move_to(node_positions[SRC_NODE + 1]), run_time=2)
-    
-#     def optimize(self, n_iters, n_ants, degradation_factor=0.5, q=1, use_elitism=False):
-#         traversal_history = []
-#         graph_cpy = self.graph.copy()
-#         best_cost = float("inf")
-#         best_cycle = None
-#         for _ in range(n_iters):
-#             ant_cycles = [graph_cpy.traverse(randint(0, self.n_cities - 1)) for _ in range(n_ants)]
-#             if use_elitism and best_cycle:
-#                 ant_cycles.append((best_cycle, best_cost))
-            
-#             traversal_history.append(ant_cycles)
-
-#             ant_cycles.sort(key=lambda x: x[1])
-#             if use_elitism and best_cycle and ant_cycles[0][1] < best_cycle[1]:
-#                 best_cycle = ant_cycles[0]
-                
-#             for cycle, total_cost in ant_cycles:
-#                 best_cost, best_cycle = self.__update_best_cycle(cycle, total_cost, best_cycle, best_cost)
-#                 self.__update_pheromone(graph_cpy, cycle, q, total_cost, degradation_factor)
-
-#         return best_cycle, best_cost, traversal_history
-    
-#     def __update_pheromone(self, graph_cpy, cycle, q, total_cost, degradation_factor):
-#         delta = q/total_cost
-#         for i in range(len(cycle) - 1):
-#             graph_cpy.adj_matrix[cycle[i], cycle[i+1], 1] += delta
-        
-#         graph_cpy.adj_matrix[cycle[-1], cycle[0], 1] += delta
-#         graph_cpy.adj_matrix[:, :, 1] *= degradation_factor
-
-    
-#     def __update_best_cycle(self, cycle, cycle_cost, best_cycle, best_cost):
-#         if cycle_cost < best_cost:
-#             return cycle_cost, cycle
-#         return best_cost, best_cycle
-    
-
-
---
+++ Ant_Colony/visualizer.py
@@ -0,0 +1,219 @@
+from graph import TSPGraph
+import adjacency_mat_generator
+import networkx as nx
+import matplotlib.pyplot as plt
+from random import randint
+from manim import *
+import numpy as np
+
+class AntColonyVisualizer(Scene):
+    def __init__(self):
+        super().__init__()
+        self.N_CITIES = 4
+        self.N_ANTS = 2
+        self.N_ITERS = 2
+        self.ADJ_MAT = adjacency_mat_generator.adjacency_matrix_test1()
+
+        self.graph = TSPGraph(self.ADJ_MAT, self.N_CITIES, alpha=1, beta=1)
+        self.n_cities = self.N_CITIES
+        self.nodes = [i for i in range(self.n_cities)]
+        self.edges = self.__make_edges(self.n_cities)
+
+    def __make_edges(self, n):
+        edges = []
+        for i in range(n):
+            for j in range(i+1, n):
+                edges.append((i, j))
+                
+        return edges
+    
+    # def __make_tuples_edges(self, cycle):
+    #     edges = []
+    #     for i in range(1, cycle):
+    #         edges.append(cycle[i-1], cycle[i])
+        
+    #     return edges
+    
+    def __make_pheromones_intensities_edge_map(self, adj_matrix):
+        edge_phero = {}
+        # for i in range(self.n_cities):
+        #     for j in range(i+1, self.n_cities):
+        #         edge_phero[(i, j)] = adj_matrix[i][j]
+        
+        # return edge_phero
+    
+    def __setup_camera(self, width, height):
+        self.camera.frame_width = width        
+        self.camera.frame_height = height
+
+    def __populate_ant_moves(self, ant_moves, traversal_history, node_positions):
+        for i in range(self.N_ITERS):
+            nth_iteration_cycles = traversal_history[i]
+            for j in range(len(nth_iteration_cycles)):
+                jth_ant = nth_iteration_cycles[j]
+                cycle = jth_ant[0]
+                
+                for k in range(len(cycle)):
+                    ant_moves[i][k].append(node_positions[cycle[k]])
+                ant_moves[i][-1].append(node_positions[cycle[0]])
+
+    def __reset_ant_positions(self, ant_dots):
+        remove_dots = [FadeOut(ant_dots[k]) for k in range(self.N_ANTS)]
+        ant_dots = [Dot(color=ManimColor.from_hex("#FF0000")).scale(2).set_z_index(10) for _ in range(self.N_ANTS)]
+        create_dots = [Create(ant_dots[k]) for k in range(self.N_ANTS)]
+        self.play(remove_dots, create_dots, runtime=3)
+
+    def construct(self):
+        g = Graph(self.nodes, self.edges, layout="spring", layout_scale=12,
+                  labels=True, vertex_config={'radius':0.5})
+        self.__setup_camera(40, 30)
+        top_center = [-15, 14, 0]
+
+        
+        ant_dots = [Dot(color=ManimColor.from_hex("#FF0000")).scale(2).set_z_index(10) for _ in range(self.N_ANTS)]
+        self.add(*ant_dots)
+
+        ant_moves = [[[] for _ in range(self.n_cities + 1)] for _ in range(self.N_ITERS)] # each iteration has n movements = n_cities (for each cycle)
+        iteration_text = Tex(f"Iteration 0")
+
+        node_positions = {label: dot.get_center() for label, dot in g.vertices.items()}
+        _, _, traversal_history, pheromone_intensities = self.optimize(n_iters=self.N_ITERS, n_ants=self.N_ANTS, degradation_factor=0.1)
+        # edge_phero = self.__make_pheromones_intensities_edge_map(pheromone_intensities)
+        print(pheromone_intensities)
+        # print(edge_phero)
+
+        # graph_lines = g.edges # (i, j): Line object -------> where i, j are vertices
+        # edge_labels = []
+        # for i, edge in enumerate(self.edges):
+        #     txt = Tex('1.00', color='#00C8FF')
+        #     txt.set_z_index(10)
+        #     self.add(txt)
+        #     edge_labels.append(txt)
+
+        # # self.add(*edge_labels)
+
+        # self.__populate_ant_moves(ant_moves, traversal_history, node_positions)
+
+        # # animated scenes
+        # self.play(Create(g), runtime=2)
+
+        # for txt, e in zip(edge_labels, self.edges):
+        #     line = graph_lines[e]
+        #     line_direction = line.get_end() - line.get_start()
+        #     offset = 1.0 * (line_direction / np.linalg.norm(line_direction))
+        #     phero = str(round(edge_phero[e], 2))
+        #     self.play(txt.animate.become(Tex(phero)).move_to(line.get_start() + offset).set_z_index(10))
+            
+        # edge_recolor = [graph_lines[edge].animate.set_color(ManimColor.from_rgb((0.0, 0.2, 1.0))).set_stroke(width=10) for edge in self.edges]
+        # self.play(edge_recolor)
+        
+        # for i in range(0, self.N_ITERS):
+        #     self.play(iteration_text.animate.become(Tex(f"Iteration {i}")).move_to(top_center), runtime=2)
+        #     for j in range(self.n_cities + 1):
+        #         next_ant_positions = ant_moves[i][j]
+        #         moves = [ant_dots[k].animate.move_to(next_ant_positions[k]) for k in range(self.N_ANTS)]
+        #         self.play(moves, runtime=1)
+
+        #     # Reset dots
+        #     remove_dots = [FadeOut(ant_dots[k]) for k in range(self.N_ANTS)]
+        #     ant_dots = [Dot(color=ManimColor.from_hex("#FF0000")).scale(2).set_z_index(10) for _ in range(self.N_ANTS)]
+        #     create_dots = [Create(ant_dots[k]) for k in range(self.N_ANTS)]
+        #     self.play(remove_dots, create_dots, runtime=3)
+            
+        #     # Update edges
+        #     edge_recolor = []
+        #     for edge in self.edges:
+        #         line = graph_lines[edge]
+        #         line_phero_val = float(edge_phero[edge]) * 10.0
+
+        #         # R, G, B = line.get_style()['stroke_color'][0].to_rgb()
+        #         color = interpolate_color(BLUE, GREEN, line_phero_val).to_rgb() # the greater the phero intensity the closer it is to green
+        #         color[0] = 0 # make red = 0
+        #         color[1] -= 0.1 # color less green
+        #         color[2] += 0.1 # make blue bluer
+
+        #         # edge_recolor.append(line.animate.set_color(ManimColor.from_rgb((R, G, B))).set_stroke(width=10))
+        #         edge_recolor.append(line.animate.set_color(ManimColor.from_rgb(color)).set_stroke(width=(line_phero_val*20 + 10)))
+            
+        #     animations = []
+        #     self.play(edge_recolor)
+        #     for txt, e in zip(edge_labels, self.edges):
+        #         line = graph_lines[e]
+        #         line_direction = line.get_end() - line.get_start()
+        #         offset = 1.0 * (line_direction / np.linalg.norm(line_direction))
+        #         phero = str(round(edge_phero[e], 2))
+        #         self.play(txt.animate.become(Tex(phero)).move_to(line.get_start() + offset).set_z_index(10))
+        #     # self.play(animations)
+
+        # TODO: Configure edges, add colors (FadeToColor), add labels
+        # TODO: Make everything disappear (fade in/fade out) and display best path
+        # TODO: Modify Text writing animation.Write
+        # TODO: write cost on edges of best cycle
+        # self.add(*iteration_text)
+    
+    def optimize(self, n_iters, n_ants, degradation_factor=0.5, q=1, use_elitism=False):
+        traversal_history = []
+        intensity_history = []
+        graph_cpy = self.graph.copy()
+        best_cost = float("inf")
+        best_cycle = None
+        intensity_history.append(graph_cpy.adj_matrix[:, :, 1])
+        for _ in range(n_iters):
+            ant_cycles = [graph_cpy.traverse(randint(0, self.n_cities - 1)) for _ in range(n_ants)]
+            if use_elitism and best_cycle:
+                ant_cycles.append((best_cycle, best_cost))
+            
+            traversal_history.append(ant_cycles)
+
+            ant_cycles.sort(key=lambda x: x[1])
+            if use_elitism and best_cycle and ant_cycles[0][1] < best_cycle[1]:
+                best_cycle = ant_cycles[0]
+                
+            graph_cpy.adj_matrix[:, :, 1] *= degradation_factor
+            for cycle, total_cost in ant_cycles:
+                best_cost, best_cycle = self.__update_best_cycle(cycle, total_cost, best_cycle, best_cost)
+                self.__update_pheromone(graph_cpy, cycle, q, total_cost, degradation_factor)
+            intensity_history.append(graph_cpy.adj_matrix[:, :, 1])
+
+        return best_cycle, best_cost, traversal_history, intensity_history
+    
+    def __update_pheromone(self, graph_cpy, cycle, q, total_cost, degradation_factor):
+        delta = q/total_cost
+        for i in range(len(cycle) - 1):
+            graph_cpy.adj_matrix[cycle[i], cycle[i+1], 1] += delta
+        
+        graph_cpy.adj_matrix[cycle[-1], cycle[0], 1] += delta
+
+    
+    def __update_best_cycle(self, cycle, cycle_cost, best_cycle, best_cost):
+        if cycle_cost < best_cost:
+            return cycle_cost, cycle
+        return best_cost, best_cycle
+
+
+class DynamicGraphColoring(Scene):
+    def construct(self):
+        nodes = [i for i in range(5)]
+        edges = [(0, 1), (1, 2), (2, 3), (3, 0), (2, 4)]
+
+        graph = Graph(nodes, edges, layout="circular")
+        self.play(Create(graph))
+        self.play(FadeToColor(graph.edges[(0, 1)], color=BLUE))
+        # self.play(FadeOut(graph, scale=0.5))
+
+class LineT(Scene):
+    def construct(self):
+
+        my_line = Line(LEFT, RIGHT, color=BLUE, stroke_width=2)
+
+        # Animate color change to red with a duration of 2 seconds
+        self.play(my_line.animate.set_color(RED).set_stroke(width=5), run_time=2)
+
+        self.add(my_line)
+
+class Table1(Scene):
+    def construct(self):
+        row_labels = [Tex(str(i)) for i in range(10)]
+        col_labels = [Tex(str(i)) for i in range(10)]
+
+        table = MobjectTable([[row_labels, col_labels]])
